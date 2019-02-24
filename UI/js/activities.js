c50ddcb88445ce957a7534f33b7b0d371e8e1aa1
otaigbe <stanlex4400@gmail.com>
2019-02-25 00:03:50 +0100

    [Feature #164188049]Added the send email page

---
+++ UI/js/activities.js
@@ -0,0 +1,27 @@
+function createComposeWindow(e){
+	let modal = `<div id="myModal" class="modal">
+<div id="bar"><button type="button" class="close-btn"><i class="fa fa-close"></i>
+</button></div><div id="details"><input type="text" name="to" placeholder="To"><br><input type="text" name="Cc" placeholder="Cc"></div><div id="editor"></div><button type="button" class="save">Save As Draft</button><button type="button" class="send">Send</button></div>`;
+	modal = document.createRange().createContextualFragment(modal);
+   document.getElementById('composeWindow').appendChild(modal);
+	e.target.disabled= true;
+	var options = {
+  debug: 'info',
+  modules: {
+    toolbar: '#toolbar'
+  },
+  placeholder: 'Compose an epic...',
+  readOnly: true,
+  theme: 'bubble'
+};
+var editor = new Quill('#editor', {theme: 'snow'});
+}
+
+function closeComposeWindow(e){
+e.target.parentElement.parentElement.parentElement.removeChild(e.target.parentElement.parentElement);
+document.getElementById('compose').disabled = false;
+}
+
+
+$('body').on('click', '#compose', createComposeWindow);
+$('body').on('click', '.close-btn', closeComposeWindow);
