3b9de0f6af83c2886aeefb2292a60351317edfd5
otaigbe <stanlex4400@gmail.com>
2019-02-27 04:33:05 +0100

    [Feature #164242762] Addedd the sent mails and retract button UI components

---
+++ UI/js/activities.js
@@ -0,0 +1,42 @@
+function createComposeWindow(e) {
+	on();
+	let modal = `<div id="myModal" class="modal">
+<div class="bar"><button type="button" class="close-btn"><i class="fa fa-close"></i>
+</button></div><div id="details"><input type="text" name="to" placeholder="To"><br><input type="text" name="Cc" placeholder="Cc">
+<input type="text" name="topic" placeholder="Topic">
+</div><div id="editor"></div><button type="button" class="save">Save As Draft</button><button type="button" class="send">Send</button></div>`;
+	modal = document.createRange().createContextualFragment(modal);
+	document.getElementById('composeWindow').appendChild(modal);
+	e.target.disabled = true;
+	var editor = new Quill('#editor', {
+		theme: 'snow'
+	});
+}
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function closeComposeWindow(e) {
+	off();
+e.target.parentElement.parentElement.parentElement.removeChild(e.target.parentElement.parentElement);
+	document.getElementById('compose').disabled = false;
+}
+
+
+
+$('body').on('click', '#compose', createComposeWindow);
+$('body').on('click', '.close-btn', closeComposeWindow);
+
+function createReply() {
+	let modal = `<div id="replyModal" class="replymodal">
+<div class="bar"><button type="button" class="close-btn-reply"><i class="fa fa-close"></i>
+</button></div><div id="details"><input type="text" name="to" placeholder="To"><br><input type="text" name="Cc" placeholder="Cc">
+<input type="text" name="topic" placeholder="Topic"></div>
+<div class="replyeditor"></div><button type="button" class="send" >Send</button></div>`;
+	return modal;
+}
