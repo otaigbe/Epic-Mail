80d0063bea0ccfe2c43674cc3ac35dcee169f37c
otaigbe <stanlex4400@gmail.com>
2019-03-17 21:14:23 +0100

    [Feature #164684169] Added the delete group by id endpoint

--- src/controller/groups.js
+++ src/controller/groups.js
@@ -16,7 +16,7 @@ export default class GroupsController {
    * @returns {Object} Success or failure message
    */
   static async createGroup(req, res) {
-      console.log(req.body);
+    console.log(req.body);
     const result = Joi.validate(req.body, schema.group);
     if (result.error === null) {
       console.log('joi validated');
@@ -26,12 +26,24 @@ export default class GroupsController {
       const args = [group.groupName, group.creator];
       const dbOperationResult1 = await dbhelpers.performTransactionalQuery(queries.checkIfUserAlreadyHasGroupWithGroupName, args);
       if (dbOperationResult1.rowCount > 0) {
-          return res.status(201).json(response.groupFailure(`You Already have a group with name ${group.groupName}! Chooose a different group name`, 400));
+        return res.status(400).json(response.groupFailure(`You Already have a group with name ${group.groupName}! Chooose a different group name`, 400));
       }
       const dbOperationResult = await dbhelpers.performTransactionalQuery(queries.createGroup, args);
-      return res.status(201).json(response.groupSuccess(group, 'Group created!', 200));
-    } else {
+      return res.status(201).json(response.groupSuccess(group, `Group with id ${dbOperationResult.rows[0].groupid} has been created!`, 200));
+    }
     errorHandler.validationError(res, result);
+  }
+
+  static async deleteGroupById(req, res) {
+    const args = [req.params.groupId, 'otaigbe@epicmail.com'];
+    const dbOperationResult = await dbhelpers.performTransactionalQuery(queries.checkIfUserOwnsTheGroupAboutToBeDeleted, args);
+    if (dbOperationResult.rowCount === 1) {
+      const dbOperationResult2 = await dbhelpers.performTransactionalQuery(queries.deleteGroupById, args);
+      return res.status(200).json(response.groupSuccess(null, `Deleted group with id of ${req.params.groupId}`, 200));
+    }
+    if (dbOperationResult.rowCount === 0) {
+      return res.status(404).json(response.failure(`Couldn't find group with id ${req.params.groupId} belonging to you`, null, 404));
     }
   }
+
 }
---
+++ src/tests/v1/groups.js
@@ -0,0 +1,51 @@
+/* eslint-disable prefer-destructuring */
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('Testing the groups Endpoint', () => {
+  describe('Testing the create and own group by user otaigbe Endpoint', () => {
+    it('should create a group successfully for user otaigbe', async () => {
+      const res = await chai.request(app).post('/api/v1/groups').type('form').send({
+        groupname: 'friends',
+        creator: 'otaigbe@epicmail.com',
+      });
+      chai.expect(res).to.have.status(201);
+      chai.expect(res.body).to.have.property('status');
+      chai.expect(res.body).to.have.property('data');
+      chai.expect(res.body).to.have.property('message');
+    });
+    it('should create a group successfully for user osas422 with same group name as the one created by user otaigbe', async () => {
+      const res = await chai.request(app).post('/api/v1/groups').type('form').send({
+        groupname: 'friends',
+        creator: 'osas422@epicmail.com',
+      });
+      chai.expect(res).to.have.status(201);
+      chai.expect(res.body).to.have.property('status');
+      chai.expect(res.body).to.have.property('data');
+      chai.expect(res.body).to.have.property('message');
+    });
+    // eslint-disable-next-line no-template-curly-in-string
+    it('should return a "You Already have a group with name this group name! Chooose a different group name" message', async () => {
+      const res = await chai.request(app).post('/api/v1/groups').type('form').send({
+        groupname: 'friends',
+        creator: 'osas422@epicmail.com',
+      });
+      chai.expect(res).to.have.status(400);
+      chai.expect(res.body).to.have.property('status');
+      chai.expect(res.body).to.have.property('error');
+    });
+  });
+
+  describe('Testing the delete group by id endpoint', () => {
+    it('should delete a group with provided id', async () => {
+      const res = await chai.request(app).delete('/api/v1/groups/1');
+      chai.expect(res).to.have.status(200);
+      chai.expect(res.body).to.have.property('status');
+      chai.expect(res.body).to.have.property('message');
+    });
+  });
+});
